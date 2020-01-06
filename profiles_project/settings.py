559b5ce404ade982b4fbebf257256742c6c663b9
Mhirley Lopes <mhiloca@gmail.com>
2020-01-06 12:00:31 -0300

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '64q4(0%etpqf^)r7cuco%b=dcy=@=cx)t%f_j0ycwjq6s2g*qb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', 1))
 
 ALLOWED_HOSTS = []
 
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
